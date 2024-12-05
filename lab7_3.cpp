a066704eba4ad3c8481b0eab464e8f890c553444
Twilight9990 <donedoneeiei@gmail.com>
2024-12-05 19:34:21 +0700

    lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,8 @@
-#include<iostream>
-using namespace std;
-
 char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+	if(x=='A'){
+		return 'Z';
+	}else if (x>90||x<65){
+		return '0';
+	}
+	return char(x-1);
 }
